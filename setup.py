f7ef8a4542da161d35b8ba4d9c670c110a7ab493
Keith <keith@tauri-tec.com>
2017-06-22 20:55:00 +0100

    Increasing the version number to include Neil's fixes.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md')) as f:
 
 setup(
     name='dotmailer',
-    version='0.4.0',
+    version='0.4.1',
     description='DotMailer API wrapper',
     long_description=long_description,
 
